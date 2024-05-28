736b7e1e80a14cd97436c616bd471c368bbe7a03
Olive020 <a0977062020@gmail.com>
2024-05-28 23:27:37 +0800

    feat: python file

---
+++ README.dataset.txt
@@ -0,0 +1,6 @@
+# AIOT plant > 2024-05-28 9:11pm
+https://universe.roboflow.com/aiot-plant/aiot-plant
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ train.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+# 加載 YOLOv8 模型
+model = YOLO('yolov8n.pt')  # 替換為您的模型文件路徑
+
+# 訓練模型
+model.train(data='config.yaml', epochs=50, batch=8)  # 將 batch_size 改為 batch
+
+# 評估模型
+results = model.val()
+
+# 打印評估結果
+print(results)
+
+# 使用模型進行推理
+results = model('test/')  # 替換為您的測試圖像資料夾路徑
+results.save()
+
